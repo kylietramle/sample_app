f11e98d438c4b52899e69f31e91bbc6f8d016e93
Kylie Le <kylie.tram.le@gmail.com>
2016-04-11 19:34:03 -0700

    calling instance variable from controller to view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,11 @@ class PagesController < ApplicationController
   end
 
   def about
+    @current_time = Time.now
   end
 
   def contact_us
+    names = ["Mark", "Sami", "Natasha"]
+    @one_name = names.sample
   end
 end
